2423a847d36152fd9d164307bbb4dc3ca9141813
Ali Demirbaş <ali.ds@outlook.com.tr>
2023-06-22 20:20:53 +0300

    .

--- src/P.HttpService/ServiceCollectionExtensions.cs
+++ src/P.HttpService/ServiceCollectionExtensions.cs
@@ -5,22 +5,22 @@ namespace P.HttpService
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddHttpService<THttpService, TImplementation, THttpServiceConfiguration>(this IServiceCollection services, IConfiguration configuration, string httpServiceConfigurationsSectionName)
+        public static IServiceCollection AddHttpService<THttpService, TImplementation, THttpServiceConfiguration>(this IServiceCollection services, IConfiguration configuration, string httpServiceConfigurationSectionName)
             where THttpServiceConfiguration : HttpServiceConfiguration
             where THttpService : class
             where TImplementation : HttpService, THttpService
         {
-            RegisterHttpServiceConfiguration<THttpServiceConfiguration>(services, configuration, httpServiceConfigurationsSectionName);
+            RegisterHttpServiceConfiguration<THttpServiceConfiguration>(services, configuration, httpServiceConfigurationSectionName);
 
             services.AddHttpClient<THttpService, TImplementation>();
 
             return services;
         }
 
-        private static void RegisterHttpServiceConfiguration<T>(IServiceCollection services, IConfiguration configuration, string httpServiceConfigurationsSectionName)
+        private static void RegisterHttpServiceConfiguration<T>(IServiceCollection services, IConfiguration configuration, string httpServiceConfigurationSectionName)
             where T : HttpServiceConfiguration
         {
-            var configSection = $"{httpServiceConfigurationsSectionName}:{typeof(T).Name}";
+            var configSection = $"httpServiceConfigurations:{httpServiceConfigurationSectionName}";
 
             services.Configure<T>(configuration.GetSection(configSection));
         }
